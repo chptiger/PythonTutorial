9ee10df6584fff83d04e8da82e3c0d7b83f83701
Xin Xu <thomas.java863@gmail.com>
2020-05-26 16:26:37 -0700

    Python Basic || keyword arguments

---
+++ Functions.py
@@ -0,0 +1,9 @@
+def great_user(name):
+    print(f'Hi {name}!')
+    print('Welcome aboard')
+
+
+print('Start')
+great_user("John Smith")
+great_user("Mary")
+print('Finish')
---
+++ keywordArguments.py
@@ -0,0 +1,8 @@
+def great_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome aboard')
+
+
+print('Start')
+great_user(last_name="Smith", first_name="John") # keyword arguments add readability fo the code. It's better than Java
+print('Finish')
