4e01f0f327abf66f89d3157edcf80e0582efeed1
Xin Xu <thomas.java863@gmail.com>
2020-05-26 11:34:19 -0700

    Python Basic

--- Dictionary.py
+++ Dictionary.py
@@ -1,1 +1,22 @@
-# key, value pairs
+# key, value pairs, key unique
+customer = {
+    "name" : "John Smith",
+    "age" : 40,
+    "is_verified" : True
+}
+print(customer)
+print(customer["name"])
+print(customer.get("name"))
+
+# ask phone number
+dic = {
+    "1" : "One",
+    "2": "Two",
+    "3": "Three"
+}
+
+phone = input("Phone: ");
+output = ""
+for item in phone:
+    output +=dic.get(item, "!")
+print(output)
---
+++ emojiConverter.py
@@ -0,0 +1,11 @@
+message = input("> ")
+words = message.split(' ')
+emojis = {
+    ":)" : "😄",
+    ":(" : "😟"
+}
+output = ""
+for word in words:
+    output +=emojis.get(word, word) + " "
+print(output)
+print(emojis)
